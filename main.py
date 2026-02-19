5ac7bd864d5ab44e19c36bdd1c541c0005a4a2f3
abheemid <arati_bheemidi@yahoo.com>
2026-02-18 19:28:06 -0600

    changes

--- main.py
+++ main.py
@@ -7,8 +7,8 @@
 # --- Imports ---
 # LangChain components for LLM interaction and prompt management
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain import LLMChain
-from langchain import PromptTemplate
+from langchain_classic.chains import LLMChain
+from langchain_core.prompts import PromptTemplate
 
 # Streamlit for the web UI, os for environment variable management
 import streamlit as st
